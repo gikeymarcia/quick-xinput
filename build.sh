e9000b4715888dcad7f72f1bfeca6259d70f21a2
Mikey Garcia <gikeymarcia@gmail.com>
2021-07-12 18:38:44 -0700

    build script for release

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# enter development environment
+source ./enter-the-dojo.sh
+
+# remove old builds
+rm -rf ./build/
+rm -rf ./dist/
+
+# build source archive and wheel
+python ./setup.py sdist bdist_wheel
+
+# display tar contents
+tar tzf dist/*tar.gz
+
+# to upload to PyPi
+# twine upload dist/*
